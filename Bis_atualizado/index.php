829a65b72284dd0aaf7c8870c8b9107878bd9cdc
AngeloVitorio <42278661+AngeloVitorio@users.noreply.github.com>
2018-09-11 16:55:49 -0300

    Add files via upload

---
+++ Bis_atualizado/index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<title>Bis</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="shortcut icon" href="img/img.jpg">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<nav class="navbar navbar navbar-fixed-top ">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="index.php"><img src="img/icon1.png" alt="BIS" id="icu" class="brand-logo" style="top:5px; margin-left:-3px;"
+					 width="135" height="49"></a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="home.php">HOME</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">HARDWARE</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Manutenção</a></li>
+						<li><a href="#">Suporte</a></li>
+					</ul>
+				<li><a href="cftv.php">CFTV</a></li>
+				<li><a href="software.php">SOFTWARE</a></li>
+				<li><a href="solucaoemauto.php">SOLUÇÃO EM AUTOMAÇÃO</a></li>
+				</li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#"><span class="glyphicon glyphicon"></span>(85) 9 4002-8922</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-user"></span> Cadastrar-se</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+
+			</ul>
+		</div>
+	</nav>
+
+	<!--CODIGO DO ISLID-->
+	<div class="container-fluid1">
+		<div id="myCarousel" class="carousel" style="top:-20px;" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" class="active"></li>
+				<li data-target="#myCarousel"></li>
+				<li data-target="#m"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="img/e10.jpg" style="width:100%;">
+					<div class="carousel-caption">
+						<p id="titulo">Suporte</p>
+						<p id="paragrafo"></p>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="img/e10.jpg" style="width:100%;">
+					<div class="carousel-caption">
+						<P id="titulo">Segurança</P>
+						<p id="paragrafo"></p>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="img/e10.jpg" style="width:100%;">
+					<div class="carousel-caption">
+						<P id="titulo">Desenvolvimento</P>
+						<p id="paragrafo"></p>
+					</div>
+				</div>
+
+			</div>
+			<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only"></span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only"></span>
+			</a>
+		</div>
+	</div>
+	<!-- Fim codigo slid-->
+	<br>
+	<!-- Inicio codigo scrool-->
+	<div class="bgimg-1" id="opaci0">
+		<div class="caption">
+			<span class="border">Conheça-nos</span>
+		</div>
+	</div>
+
+	<div style="color: #151515;background-color:whitch;text-align:center;padding:50px 80px;text-align: justify;">
+		<h3 style="text-align:center;">Banco de informatização e Serviços.</h3>
+		<p>BIS – Banco de Informatização e Serviço Ltda. Foi instituído em 1996 e vem prestando serviços na área de
+			consultoria, desenvolvimento de sistema e parcerias em hardware e redes. Hoje temos sistemas de gerenciamento para
+			diversos setores de atividade, tais como distribuidoras, lojas, fábricas, pousadas, prestadoras de serviços, etc.
+			Como o objetivo de atender cada vez melhor, o BIS procura levar a seus clientes novidades sobre produtos e serviços
+			que ajudem a seu crescimento no mercado.
+
+			<div class="bgimg-2" id="opaci1">
+				<div class="caption">
+					<span class="border" style="background-color:#E0F8F7;font-size:27px;color:whitch;padding:15px 37px;">Hardware</span>
+				</div>
+			</div>
+
+			<div style="position:relative;">
+				<div style="color:#FFFFFF;background-color:#0B243B;font-size:14.9px;text-align:center;padding:30px 60px;text-align:justify;">
+					<p>
+						<h3 id="cor">Manutenção em computadores</h3>A manutenção DE computadores é um conjunto de ações desencadeadas,
+						visando solucionar um problema diagnosticado no computador, ou antever um problema relacionado a software e/ou
+						hardware.
+					</p>
+					<p>
+						<h3 id="cor">Redes estruturadas</h3>Conjunto de componentes tais como cabos, distribuidores, conectores, painéis
+						de interligação. Redirecionados com intuito de equipar um caminho de transmissão entre pontos de uma rede.
+					</p>
+					<p>
+						<h3 id="cor">Contrato de manutenção empresarial</h3>Ações de acompanhamento remoto e visitas programadas que
+						visam manter a estabilidade e segurança dos sistemas ligados a TI. O cliente conta com uma equipe de TI à
+						disposição da empresa.
+					</p>
+				</div>
+			</div>
+			<div class="bgimg-3" id="opaci2">
+				<div class="caption">
+					<span class="border" style="background-color:#E0F8F7;font-size:26px;color:black;padding:14px 72px;letter-spacing:5px;">CFTV</span>
+				</div>
+			</div>
+
+			<div style="position:relative;">
+				<div style="color:#FFFFFF;background-color:#0B243B;font-size:14.9px;text-align:center;padding:30px 70px;text-align:justify;">
+					<h3 id="cor">Conceito CFTV</h3>
+					<p>Conceitos e formatos da televisão (CFTV). É um sistema de televisão que distribui sinais provenientes de
+						câmeras localizadas em locais específicos, para um ou mais pontos de visualização.
+						Sendo constituído por câmera(s), meio de transmissão e monitor. </p>
+				</div>
+			</div>
+			<!--Fim codigo scrool-->
+			<br>
+			<br>
+
+			<a href="https://www.dupe.com.br/" target="_blank"><img src="img/logo1.png" class="responsive" width="400" height="200"></a>
+
+</body>
+
+</html>
---
+++ Bis_atualizado/karai.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.responsive {
+    width: 100%;
+    max-width: 250px;
+    max-height:350px;
+    height: 100%;
+    border-radius:40%;
+}
+</style>
+</head>
+<body>
+
+<a href="https://www.dupe.com.br/"  target="_blank"><img src="img/logo1.png" class="responsive"  width="400" height="200"></a>
+
+</body>
+</html>
